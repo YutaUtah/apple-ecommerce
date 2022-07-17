1e787b72e01d346f6d2b71a1cde044587a6cbdb8
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-07-17 23:47:17 +0900

    added a couple of main components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css';
 import { Link , Outlet } from "react-router-dom";
 import Header from "./components/Header.jsx"
+import Category from "./components/Category.jsx"
+
+import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <div className="App">
       <Header/>
-      
-      {/* <Link to="/invoices">Invoices</Link>{ " " }
-      <Link to="/expenses">Expenses</Link> */}
+      <Category/>
     </div>
   );
 }
