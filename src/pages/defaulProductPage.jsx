457ab28d9fb404742aefee573677c032914b8a82
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-07-18 22:35:33 +0900

    reuse category component

--- src/pages/defaulProductPage.jsx
+++ src/pages/defaulProductPage.jsx
@@ -1,22 +1,13 @@
 import Category from "../components/Category.jsx";
-import productList from "../data/productList.js";
-import { Box, Stack, Typography } from "@mui/material";
-import { Container } from "@mui/system";
-
-const defaultProductPage = () => {
-  const keys = Object.keys(productList);
 
 
-return (
+const DefaultProductPage = () => {
+
+  return (
     <div>
-      <Category productList={productList} category="Mac" />
-      <Category productList={productList} category="iPhone" />
-      <Category productList={productList} category="iPad" />
-      <Category productList={productList} category="Watch" />
+      <Category/>
     </div>
-  )
-
-  ;
+  );
 };
 
-export default defaultProductPage;
+export default DefaultProductPage;
