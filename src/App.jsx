ee9ced1ac1479e7bd8a7ff0a001a1f9f60f56f5d
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-07-18 19:25:00 +0900

    added DefaultProductPage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import './App.css';
 import { Link , Outlet } from "react-router-dom";
 import Header from "./components/Header.jsx"
-import Category from "./components/Category.jsx"
+import DefaultProductPage from "./pages/DefaulProductPage"
 
 import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div className="App">
+    <div>
       <Header/>
-      <Category/>
+      <DefaultProductPage/>
     </div>
   );
 }
