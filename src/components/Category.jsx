804063b4278dc2a0e7cef69381c643b16422e745
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-07-18 17:40:01 +0900

    modified Category: from product list

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -4,32 +4,30 @@ import "./Category.css"
 // mui
 import { Box, Stack, Typography } from "@mui/material";
 import { Container } from "@mui/system";
+import productList from "../data/productList.js";
+
+
 
 const Category = () => {
-
+//   console.log(productList[0]["category"])
   return (
-    <div className="container ">
-      <Box sx={{ mt: 15 , overflow: "scroll" }}>
-        {/* <Typography sx={{textAlign: "start", margin: 0}} variant="h4">ストア</Typography> */}
-        <div className=" ov d-flex mt-5 ">
-        <Stack component="div" direction="row" sx={{ mt: 15 , overflow: "scroll" }}>
-          {/* {items.map((item) => <Item item={item} key={item.productName} />)} */}
+    <Container>
+      <Box sx={{ mt: 15  }}>
+        <Typography sx={{textAlign: "start", margin: 0}} variant="h4">ストア</Typography>
+        <div className="media-controller ">
+        <Stack component="div" direction="row" sx={{ mt: 5 , overflow: "scroll" }}>
+          {productList.map((item) => <Item item={item} key={item.productName} />)}
+
+          {/* <Item />
           <Item />
           <Item />
           <Item />
           <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
+          <Item /> */}
         </Stack>
         </div>
       </Box>
-    </div>
+      </Container>
   );
 };
 
