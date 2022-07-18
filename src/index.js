72ef3ddf9920488cae3c80b1d7d0061ae7c092cf
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-07-18 17:41:44 +0900

    added bootstrap

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import "jquery";
+import "popper.js/dist/umd/popper";
+import "bootstrap/dist/js/bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
