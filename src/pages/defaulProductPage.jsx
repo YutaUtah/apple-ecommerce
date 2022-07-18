9e9b17bb160b513c803e1e0e7e863feca1d9fc45
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-07-18 22:04:56 +0900

    dict -> list

--- src/pages/defaulProductPage.jsx
+++ src/pages/defaulProductPage.jsx
@@ -1,11 +1,22 @@
-import Category from "../components/Category.jsx"
+import Category from "../components/Category.jsx";
 import productList from "../data/productList.js";
-
+import { Box, Stack, Typography } from "@mui/material";
+import { Container } from "@mui/system";
 
 const defaultProductPage = () => {
-    return (
-        <Category productList={productList}/>
-    )
-}
+  const keys = Object.keys(productList);
+
+
+return (
+    <div>
+      <Category productList={productList} category="Mac" />
+      <Category productList={productList} category="iPhone" />
+      <Category productList={productList} category="iPad" />
+      <Category productList={productList} category="Watch" />
+    </div>
+  )
+
+  ;
+};
 
 export default defaultProductPage;
