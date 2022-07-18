f93fd29d1be387d0e30e5c2fa4ca7a123d44b013
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-07-18 22:04:36 +0900

    added ストア

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,25 +1,30 @@
-import React from 'react';
+import React from "react";
 // components
 import Item from "./Item.jsx";
-import "./Category.css"
+import "./Category.css";
 // mui
 import { Box, Stack, Typography } from "@mui/material";
 import { Container } from "@mui/system";
 
+const Category = ({ productList, category }) => {
 
-
-const Category = ({productList}) => {
   return (
-    <Container>
-      <Box sx={{ mt: 15 }}>
-        <Typography sx={{textAlign: "start"}} variant="h4" >ストア</Typography>
-        <div className="media-controller ">
-        <Stack component="div" direction="row" sx={{ mt: 5 , overflow: "scroll" }}>
-          {productList.map((item) => <Item item={item} key={item.productName} />)}
-        </Stack>
-        </div>
-      </Box>
+    <div>
+      <Container>
+        <Box sx={{ mt: 15 }}>
+          <Typography sx={{ textAlign: "start" }} variant="h4">
+            {category}ストア
+          </Typography>
+          <div className="media-controller ">
+            <Stack component="div" direction="row" sx={{ mt: 5, overflow: "scroll" }}>
+              {productList[category].map((item) => (
+                <Item item={item} key={item.productName} />
+              ))}
+            </Stack>
+          </div>
+        </Box>
       </Container>
+    </div>
   );
 };
 
