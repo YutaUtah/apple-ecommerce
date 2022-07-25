61bd5c8cf181b3ba5e9935cd4a74abf3039c68a9
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-07-25 19:56:29 +0900

    moved components to pages directory

--- src/pages/ProductInfo.jsx
+++ src/pages/ProductInfo.jsx
@@ -1,15 +1,15 @@
 import Header from "../components/Header.jsx";
-import productList from "../data/productList";
+import LeftProductDetail from "../components/LeftProductDetail.jsx";
+import RightProductDetail from "../components/RightProductDetail.jsx";
 
 import { styled } from "@mui/material/styles";
-import { Box, Container, Stack, Typography, Paper, Grid } from "@mui/material";
+import { Box, Container, Paper, Grid } from "@mui/material";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
   padding: theme.spacing(2),
   textAlign: "center",
-  //   color: theme.palette.text.secondary,
 }));
 
 const ProductInfo = () => {
@@ -19,35 +19,15 @@ const ProductInfo = () => {
       <Container>
         <Box sx={{ mt: 12 }}>
           <h1>Product Detail</h1>
-          <Grid container spacing={2}>
+          <Grid sx={{ mt: 2 }} container spacing={2}>
             <Grid item xs={8}>
               <Item>
-                <img
-                  src={"images/macbook-air-m1.jpeg"}
-                  height="300"
-                  width="350"
-                />
-                <h2>MacBook Air with M1 chip</h2>
-                <Item>
-                  <p>{productList.Mac[0]["description"]}</p>
-                </Item>
+                <LeftProductDetail />
               </Item>
             </Grid>
             <Grid item xs={4}>
               <Item>
-                <Box
-                  sx={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                    p: 1,
-                    m: 1,
-                    bgcolor: "background.paper",
-                    borderRadius: 1,
-                  }}
-                >
-                  <Typography variant="h6">$1000</Typography>
-                  <Typography variant="h6">$1000</Typography>
-                </Box>
+                <RightProductDetail />
               </Item>
             </Grid>
           </Grid>
