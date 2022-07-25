ebcfc5b77b0c9cf473c0e46a19280ca6872369fa
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-07-25 20:11:20 +0900

    set minHeight to be 550 for both boxes

--- src/components/LeftProductDetail.jsx
+++ src/components/LeftProductDetail.jsx
@@ -4,21 +4,32 @@ import productList from "../data/productList";
 import { styled } from "@mui/material/styles";
 import { Paper } from "@mui/material";
 
-const Item = styled(Paper)(({ theme }) => ({
+const DescriptionItem = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
   padding: theme.spacing(2),
   textAlign: "center",
 }));
 
+const ProductItem = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  display: "flex",
+  justifyContent: "space-between",
+}));
+
 const LeftProductDetail = () => {
   return (
     <div>
-      <img src={"images/macbook-air-m1.jpeg"} height="300" width="350" />
-      <h2>MacBook Air with M1 chip</h2>
-      <Item>
+      <img src={productList.Mac[0]["image"]} height="300" width="350" />
+      <ProductItem>
+        <h2>MacBook Air with M1 chip</h2>
+        <h3>${productList.Mac[0]["price"]}</h3>
+      </ProductItem>
+      <DescriptionItem>
         <p>{productList.Mac[0]["description"]}</p>
-      </Item>
+      </DescriptionItem>
     </div>
   );
 };
