991ca18292561236446feb322e4dc0f7a9ffd0fd
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-07-31 18:05:21 +0900

    added getProducfromLinkName func

--- src/pages/ProductInfo.jsx
+++ src/pages/ProductInfo.jsx
@@ -1,6 +1,10 @@
 import Header from "../components/Header.jsx";
 import LeftProductDetail from "../components/LeftProductDetail.jsx";
 import RightProductDetail from "../components/RightProductDetail.jsx";
+import productList from "../data/productList.js";
+
+import { useParams } from "react-router-dom";
+
 
 import { styled } from "@mui/material/styles";
 import { Box, Container, Paper, Grid } from "@mui/material";
@@ -14,16 +18,29 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 const ProductInfo = () => {
+  const params = useParams();
+
+  function getProductfromLinkName (productList) {
+    for (const key of Object.keys(productList)) {
+      if (typeof productList[key].find((product) => {return product.linkName == params.productName;}) === "object") {
+        return  productList[key].find((product) => {return product.linkName == params.productName;})
+      }
+    }
+  }
+
+  const product = getProductfromLinkName(productList);
+
+
   return (
     <div>
       <Header />
       <Container>
         <Box sx={{ mt: 12 }}>
-          <h1>Product Detail</h1>
+          <h1>Product Details</h1>
           <Grid sx={{ mt: 2 }} container spacing={2}>
             <Grid item xs={8} >
               <Item>
-                <LeftProductDetail />
+                <LeftProductDetail product={product}/>
               </Item>
             </Grid>
             <Grid item xs={4} >
