00bb9325994ebc9163caa35255be7b020f18f6b1
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-07-31 21:26:49 +0900

    added future plan prop

--- src/pages/defaulProductPage.jsx
+++ src/pages/defaulProductPage.jsx
@@ -1,11 +1,14 @@
 import Category from "../components/Category.jsx";
 import Header from "../components/Header.jsx";
 
+// future plan: need to filter productlist 
+import productList from "../data/productList.js";
+
 const DefaultProductPage = () => {
   return (
     <div>
       <Header />
-      <Category />
+      <Category productList={productList}/>
     </div>
   );
 };
