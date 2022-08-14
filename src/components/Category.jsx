9999cc09f66c0ca1c786bd50b3d8844639a96cb0
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-08-14 21:43:41 +0900

    refactored OneCategory

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,48 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 // components
-import Item from "./Item.jsx";
 import "./Category.css";
-// mui
-import { Box, Stack, Typography } from "@mui/material";
-import { Container } from "@mui/system";
-
 import productList from "../data/productList.js";
+import OneCategory from "./OneCategory"
 
-const OneCategory = (props) => {
 
+const Category = () => {
+  const [item, setItem] = useState('Category');
+  const [favList, setFavList] = useState([]);
+
+  const addItemInList = (item) => {
+    setItem(item)
+    const newFavList = [...favList, item];
+    setFavList(newFavList)
+  }
+  console.log(favList)
   return (
     <div>
-      <Container>
-        <Box sx={{ mt: 15 }}>
-          <Typography sx={{ textAlign: "start" }} variant="h4">
-            {props.category}ストア
-          </Typography>
-          <div className="media-controller ">
-            <Stack
-              component="div"
-              direction="row"
-              sx={{ mt: 5, overflow: "scroll" }}
-            >
-              {props.productList[props.category].map((item) => (
-                <Item item={item} key={item.productName} addItem={item => props.addItem(item)}/>
-              ))}
-            </Stack>
-          </div>
-        </Box>
-      </Container>
-    </div>
-  );
-};
-
-const Category = (props) => {
-  const [item, setItem] = useState('Category')
-  console.log(item)
-  // make sure to press fav icons
-  return (
-    <div>
-      <h1>{item}</h1>
       {Object.keys(productList).map((key) => (
-        <OneCategory productList={productList} category={key} addItem={item => setItem(item)}/>
+        <OneCategory productList={productList} category={key} addItem={item => addItemInList(item)} key={key}/>
       ))}
     </div>
   );
