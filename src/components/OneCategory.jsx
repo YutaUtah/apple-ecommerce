29daedfd94d19577c31de89511f58030b5a7c987
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-08-15 23:35:19 +0900

    props destruction

--- src/components/OneCategory.jsx
+++ src/components/OneCategory.jsx
@@ -5,22 +5,22 @@ import { Box, Stack, Typography } from "@mui/material";
 import { Container } from "@mui/system";
 
 // components
-import Item from "../components/Item.jsx";
+import { Item } from "../components/Item.jsx";
 
-
-const OneCategory = (props) => {
+export const OneCategory = (props) => {
+    const { category, productList } = props;
 
     return (
       <div>
         <Container>
           <Box sx={{ mt: 15 }}>
             <Typography sx={{ textAlign: "start" }} variant="h4">
-              {props.category}ストア
+              {category}ストア
             </Typography>
             <div className="media-controller ">
               <Stack component="div" direction="row" sx={{ mt: 5, overflow: "scroll" }}>
-                {props.productList[props.category].map((item) => (
-                  <Item item={item} key={item.productName} addItem={item => props.addItem(item)}/>
+                {productList[category].map((item) => (
+                  <Item item={item} key={item.productName} />
                 ))}
               </Stack>
             </div>
@@ -29,5 +29,3 @@ const OneCategory = (props) => {
       </div>
     );
   };
-
-export default OneCategory
