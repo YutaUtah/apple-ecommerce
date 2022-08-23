f03978d5b9c7f1b325fadac001c8cfcba1468b41
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-08-23 12:21:26 +0900

    added padding bottom

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 export const Header = () => {
   return (
     <div>
-      <Box>
+      <Box　sx={{ pb: 15 }}>
         <AppBar style={{ background: "#2E3B55" }}>
           <Toolbar>
             <Link to={'/'} >
