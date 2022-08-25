5dfde6850f3dadcb593c5f380b32719150eeb9cf
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-08-25 21:14:23 +0900

    changed the button display to remove

--- src/components/FavItem.jsx
+++ src/components/FavItem.jsx
@@ -1,7 +1,19 @@
+import React, { useContext } from "react";
 import { Box, Button, Card, CardMedia, Grid, Typography } from "@mui/material";
-import react from "react";
+
+import { UserContext } from "../providers/ContextProvider";
+
+
 
 const ItemButton = (props) => {
+  const { removeFavItem, removeCartItem} = useContext(UserContext);
+
+  const { item } = props;
+  const changeFavState = ()=> {
+    console.log('click')
+    removeFavItem();
+  }
+
     return (
       <Box
         sx={{display: "flex",　justifyContent: "center",
@@ -11,7 +23,7 @@ const ItemButton = (props) => {
             borderRadius: 1,
             }}>
         <Typography variant="h6">
-          <Button　style={{ minWidth: "170px", minHeight: "30px" }}　variant="contained"　>
+          <Button　style={{ minWidth: "170px", minHeight: "30px" }}　variant="contained" onClick={() => changeFavState()}　>
             {props.buttonTitle}
           </Button>
         </Typography>
@@ -19,36 +31,34 @@ const ItemButton = (props) => {
     );
   };
 
-// TODO: header margin to standardize
-// TODO: button to standardize
-// TODO: depending on the name, the container gets streched
-
 
 export const FavItem = (props) => {
-  const { productName, image, price } = props;
+
+  const { item } = props;
 
   const displayPrice = (price) => {
     return `$${price}`
   }
+
   return (
-    <Box sx={{ display: "flex", justifyContent: "center" }} key={productName} >
+    <Box sx={{ display: "flex", justifyContent: "center" }} key={item.productName} >
       <Card
         sx={{ minHeight: 250, minWidth: 800, mb: 1, display: "flex", alignItems: "center", justifyContent: "space-between", border: "solid" }}>
         <Grid container spacing={2}>
           <Grid item xs={6} md={4}>
-            <CardMedia component="img" height="300" width="200" image={image} alt={productName} sx={{m:2}}/>
+            <CardMedia component="img" height="300" width="200" image={item.image} alt={item.productName} sx={{m:2}}/>
           </Grid>
           <Grid item xs={6} md={5} sx={{ display: "flex", justifyContent: "center", alignItems: "center", minWidth: "370px"}} >
             <Typography gutterBottom variant="h5" component="div">
-              {productName}
+              {item.productName}
             </Typography>
           </Grid>
           <Grid item xs={6} md={3}
             sx={{display: "flex",justifyContent: "center", alignItems: "center" }}>
             <Typography variant="h5" color="text.secondary" sx={{display: "column", textAlign: "end", mx: 20 }}>
-              <div className="text-end mx-2">{displayPrice(price)}</div>
-              <ItemButton buttonTitle="Add Favorite" />
-              <ItemButton buttonTitle="Add To Cart" />
+              <div className="text-end mx-2">{displayPrice(item.price)}</div>
+              <ItemButton buttonTitle="Remove Favorite" item={item}/>
+              <ItemButton buttonTitle="Remove from Cart" />
             </Typography>
           </Grid>
         </Grid>
