cc71d02d6a13432bd71b4e3d580059ae99c05aaa
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-08-30 22:02:57 +0900

    modified grid size

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -14,21 +14,21 @@ export const CartItem = (props) => {
   const { removeCartItem, proceedPayItem, displayPrice, styles } = useContext(UserContext);
 
   return (
-    <Box sx={{ display: "flex", justifyContent: "center" }} key={item.productName} >
+    <Box sx={{ display: "flex" }} key={item.productName} >
       <Card
-        sx={{ minHeight: 250, minWidth: 800, mb: 1, display: "flex", alignItems: "center", justifyContent: "space-between", border: "solid" }}>
+        sx={{ maxWidth: "85%", minHeight: 250, minWidth: 900, mb: 1, display: "flex", alignItems: "center", justifyContent: "space-between", border: "solid" }}>
         <Grid container spacing={2}>
           <Grid item xs={6} md={4}>
             <CardMedia component="img" height="300" width="200" image={item.image} alt={item.productName} sx={{m:2}}/>
           </Grid>
-          <Grid item xs={6} md={5} sx={{ display: "flex", justifyContent: "center", alignItems: "center", minWidth: "370px"}} >
-            <Typography gutterBottom variant="h4" component="div">
+          <Grid item xs={6} md={5} sx={{ display: "flex", justifyContent: "center", alignItems: "center", minWidth: "370px" }} >
+            <Typography gutterBottom variant="h5" component="div">
               {item.productName}
             </Typography>
           </Grid>
-          <Grid item xs={6} md={3}
-            sx={{display: "flex",justifyContent: "center", alignItems: "center" }}>
-            <Typography variant="h5" color="text.secondary" sx={{display: "column", textAlign: "end", mx: 20 }}>
+          <Grid item xs={6} md={2.5}
+            sx={{display: "flex", justifyContent: "center", alignItems: "center" }}>
+            <Typography variant="h5" color="text.secondary" sx={{display: "column", textAlign: "end"}}>
               <div className="text-end mx-3">{displayPrice(item.price)}</div>
               <PrimaryButton buttonTitle="Remove from Cart" changeItemState={() => removeCartItem({item})} boxStyles={styles.productBoxStyles} buttonStyles={styles.buttonStyles}/>
               <PrimaryButton buttonTitle="Proceed Item" changeItemState={() => proceedPayItem({item})} boxStyles={styles.productBoxStyles} buttonStyles={styles.buttonStyles}/>
