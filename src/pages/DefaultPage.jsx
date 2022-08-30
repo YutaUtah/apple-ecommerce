dbd7759a3a9de91bac07ac2d272fc507f8d3bac9
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-08-30 22:05:48 +0900

    added handleChange

--- src/pages/DefaultPage.jsx
+++ src/pages/DefaultPage.jsx
@@ -1,25 +1,19 @@
 // React
-import {
-  Container,
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
-} from "@mui/material";
+import { Container } from "@mui/material";
 import React, { useContext } from "react";
 import { Category } from "../components/Category";
 import { Dropdown } from "../components/dropdowns/Dropdown";
 import { UserContext } from "../providers/ContextProvider";
 
 export const DefaultProductPage = () => {
-  const { productList } = useContext(UserContext);
+  const { productList, sortHandleChange, filterHandleChange } = useContext(UserContext);
 
   return (
     <div>
       <Container>
         <div className="d-flex justify-content-end">
-          <Dropdown name="Sort"/>
-          <Dropdown name="Filter"/>
+          <Dropdown name="Sort"   dropdownObject={{'ascending': 'Ascending', 'descending': 'Descending'}} handleChange={sortHandleChange}/>
+          <Dropdown name="Filter" dropdownObject={{'moreThanOneThousand': '$1000~'}} handleChange={filterHandleChange}/>
         </div>
         {Object.keys(productList).map((key) => (
           <Category productList={productList} category={key} key={key} />
