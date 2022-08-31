c2649edbea14d2dc87869ab6d421de45ac826689
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-09-01 00:38:46 +0900

    added right hand box

--- src/pages/CartProductPage.jsx
+++ src/pages/CartProductPage.jsx
@@ -1,30 +1,49 @@
 // React
 import React, { useContext } from "react";
 
+// css
+import "../App.css"
+
 // components
 import { UserContext } from "../providers/ContextProvider";
 import { CartItem } from "../components/CartItem";
-import { Box, Card, Container } from "@mui/material";
+import { Box, Card, Container, Typography } from "@mui/material";
+import { PrimaryButton } from "../components/buttons/PrimaryButton";
+
 
 export const CartProductPage = () => {
 
-  const { cartList } = useContext(UserContext);
-
-  console.log(cartList)
+  const { cartList, styles } = useContext(UserContext);
 
   return (
     <div>
-      <Container >
+      <Container>
         <h1>Cart List</h1>
-        <Box sx={{display: "flex", my:5}}>
+      </Container>
+      <div className="cartList-display">
+        <Box sx={{display: "flex", my: 5}}>
           { ( Object.keys(cartList).length === 0 ) ? <div><h1 className="display-5">Your cart is currently empty</h1></div>:
           <>
             <CartItemList cartList={cartList}/>
-            <Card sx={{ minWidth: "220px", maxHeight: "400px" , display: "flex", border: "solid" }}>ee</Card>
+            <Card sx={{ minWidth: "400px", maxHeight: "340px" , border: "solid"}}>
+              <Typography variant="h5" color="text.secondary" sx={{ display: "column",  m: 3 }}>
+                <div className="d-flex justify-content-between">
+                  <div>Quantity</div>
+                  <div>Quantity</div>
+                </div>
+                <div className="d-flex justify-content-between">
+                  <div>Quantity</div>
+                  <div>Quantity</div>
+                </div>
+                <div>
+                <PrimaryButton buttonTitle="Proceed Item" boxStyles={styles.productBoxStyles} buttonStyles={styles.buttonStyles}/>
+                </div>
+              </Typography>
+            </Card>
           </>
           }
         </Box>
-      </Container>
+      </div>
     </div>
   );
 };
