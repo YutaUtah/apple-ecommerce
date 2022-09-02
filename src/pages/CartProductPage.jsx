010a16daefae3c7f9199f07ab2deded3c06a1091
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-09-03 00:34:13 +0900

    added changeQuantity function

--- src/pages/CartProductPage.jsx
+++ src/pages/CartProductPage.jsx
@@ -9,14 +9,12 @@ import { UserContext } from "../providers/ContextProvider";
 import { CartItem } from "../components/CartItem";
 import { Box, Card, Container, Typography } from "@mui/material";
 import { PrimaryButton } from "../components/buttons/PrimaryButton";
-import { PriceBox } from "../components/boxes/PriceBox";
 import { TotalBox } from "../components/boxes/TotalBox";
 
 
 export const CartProductPage = () => {
 
-  const { cartList, proceedPayItem, totalPriceCalculator, styles } = useContext(UserContext);
-
+  const { cartList, proceedPayItem, changeQuantity, totalPriceCalculator, styles } = useContext(UserContext);
 
   return (
     <div>
@@ -27,10 +25,9 @@ export const CartProductPage = () => {
         <Box sx={{display: "flex", my: 5}}>
           { ( Object.keys(cartList).length === 0 ) ? <div><h1 className="display-5">Your cart is currently empty</h1></div>:
           <>
-            <CartItemList cartList={cartList}/>
-            <Card sx={{ minWidth: "400px", maxHeight: "340px" , border: "solid"}}>
-              <Typography variant="h5" color="text.secondary" sx={{ display: "column",  m: 2 }}>
-                  <PriceBox price={"11111"}/>
+            <CartItemList cartList={cartList} changeQuantity={changeQuantity}/>
+            <Card sx={{ minWidth: "400px", maxHeight: "150px" , border: "solid"}}>
+              <Typography variant="h5" color="text.secondary" sx={{ display: "column",  mx: 1 }}>
                   <TotalBox price={totalPriceCalculator(cartList)}/>
                 <div className="mx-3">
                   <PrimaryButton buttonTitle="Proceed Item" changeItemState={proceedPayItem} boxStyles={styles.productBoxStyles} buttonStyles={styles.buttonStyles}/>
@@ -47,7 +44,7 @@ export const CartProductPage = () => {
 
 const CartItemList = (props) => {
 
-  const { cartList } = props;
+  const { cartList, changeQuantity } = props;
 
   return (
     <div>
@@ -55,7 +52,7 @@ const CartItemList = (props) => {
         return (
           <div>
             {cartList[key].map((value) => (
-              <CartItem item={value} key={value.productName}/>
+              <CartItem item={value} changeQuantity={changeQuantity} key={value.productName}/>
             ))}
           </div>
         );
