878e2ecbbb8b292d1e1c8668119ec7a595b00664
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-09-02 12:36:55 +0900

    added another key quantity

--- src/data/productList.js
+++ src/data/productList.js
@@ -8,7 +8,8 @@ export const globalProductList = {
       price : 1348,
       description : 'MacBook Air with M1 is an incredibly portable laptop — it’s nimble and quick, with a silent, fanless design and a beautiful Retina display. Thanks to its slim profile and all‑day battery life, this Air moves at the speed of lightness.',
       isFavorite : false,
-      isInCart : false
+      isInCart : false,
+      quantity : 1,
     },
     {
       category : 'Mac',
@@ -18,7 +19,8 @@ export const globalProductList = {
       price : 1648,
       description : 'The M2 chip starts the next generation of Apple silicon, with even more of the speed and power efficiency of M1. So you can get more done faster with a more powerful 8‑core CPU. Create captivating images and animations with up to a 10-core GPU. Work with more streams of 4K and 8K ProRes video with the high‑performance media engine. And keep working — or playing — all day and into the night with up to 18 hours of battery life.',
       isFavorite : false,
-      isInCart : false
+      isInCart : false,
+      quantity : 1,
     },
     {
       category : 'Mac',
@@ -28,7 +30,8 @@ export const globalProductList = {
       price : 1788,
       description : 'The new M2 chip makes the 13‑inch MacBook Pro more capable than ever. The same compact design supports up to 20 hours of battery life1 and an active cooling system to sustain enhanced performance. Featuring a brilliant Retina display, a FaceTime HD camera, and studio‑quality mics, it’s our most portable pro laptop.',
       isFavorite : false,
-      isInCart : false
+      isInCart : false,
+      quantity : 1,
     },
     {
       category : 'Mac',
@@ -38,7 +41,8 @@ export const globalProductList = {
       price : 2748,
       description : 'The most powerful MacBook Pro ever is here. With the blazing-fast M1 Pro or M1 Max chip — the first Apple silicon designed for pros — you get groundbreaking performance and amazing battery life. Add to that a stunning Liquid Retina XDR display, the best camera and audio ever in a Mac notebook, and all the ports you need. The first notebook of its kind, this MacBook Pro is a beast.',
       isFavorite : false,
-      isInCart : false
+      isInCart : false,
+      quantity : 1,
     },
     {
       category : 'Mac',
@@ -48,7 +52,8 @@ export const globalProductList = {
       price : 3388,
       description : 'The most powerful MacBook Pro ever is here. With the blazing-fast M1 Pro or M1 Max chip — the first Apple silicon designed for pros — you get groundbreaking performance and amazing battery life. Add to that a stunning Liquid Retina XDR display, the best camera and audio ever in a Mac notebook, and all the ports you need. The first notebook of its kind, this MacBook Pro is a beast.',
       isFavorite : false,
-      isInCart : false
+      isInCart : false,
+      quantity : 1,
     },
     {
       category : 'Mac',
@@ -58,7 +63,8 @@ export const globalProductList = {
       price : 1548,
       description : 'Say hello to the new iMac. Inspired by the best of Apple. Transformed by the M1 chip. Stands out in any space. Fits perfectly into your life.',
       isFavorite : false,
-      isInCart : false
+      isInCart : false,
+      quantity : 1,
     },
     {
       category : 'Mac',
@@ -68,7 +74,8 @@ export const globalProductList = {
       price : 928,
       description : 'The Apple M1 chip takes our most versatile, do-it-all desktop into another dimension. With up to 3x faster CPU performance. Up to 6x faster graphics. An advanced Neural Engine for up to 15x faster machine learning. Get ready to work, play, and create on Mac mini with speed and power beyond anything you ever imagined.',
       isFavorite : false,
-      isInCart : false
+      isInCart : false,
+      quantity : 1,
     },
     {
       category : 'Mac',
@@ -78,7 +85,8 @@ export const globalProductList = {
       price : 2788,
       description : 'Mac Studio is an entirely new Mac desktop. It packs outrageous performance, extensive connectivity, and new capabilities into an unbelievably compact form, putting everything you need within easy reach and transforming any space into a studio. And it all starts with your choice of the ferociously fast M1 Max or the all-new M1 Ultra — the most powerful chip ever in a personal computer.',
       isFavorite : false,
-      isInCart : false
+      isInCart : false,
+      quantity : 1,
     },
   ],
   'iPhone': [
@@ -90,7 +98,8 @@ export const globalProductList = {
       price : 1228,
       description : 'iPhone 13 Pro takes a huge leap forward, bringing incredible speed to everything you do and dramatic new photo and video capabilities — all in two great sizes.',
       isFavorite : false,
-      isInCart : false
+      isInCart : false,
+      quantity : 1,
     },
     {
       category : 'iPhone',
@@ -98,9 +107,10 @@ export const globalProductList = {
       image : 'images/iphone13.jpeg',
       productName : 'iPhone 13',
       price : 988,
-      description : 'A lightning‑fast chip. A leap in battery life. And all‑new photo and video capabilities. iPhone 13 lets you do things you never could before — in two great sizes.',
+      description : 'A lightning-fast chip. A leap in battery life. And all‑new photo and video capabilities. iPhone 13 lets you do things you never could before — in two great sizes.',
       isFavorite : false,
-      isInCart : false
+      isInCart : false,
+      quantity : 1,
     },
     {
       category : 'iPhone',
@@ -110,7 +120,8 @@ export const globalProductList = {
       price : 578,
       description : 'At the heart of iPhone SE you’ll find the same superpowerful A15 Bionic chip that’s in iPhone 13.',
       isFavorite : false,
-      isInCart : false
+      isInCart : false,
+      quantity : 1,
     },
     {
       category : 'iPhone',
@@ -120,7 +131,8 @@ export const globalProductList = {
       price : 868,
       description : 'A superpowerful chip. 5G speed. An advanced dual‑camera system. A Ceramic Shield front that’s tougher than any smartphone glass. And a bright, beautiful OLED display. iPhone 12 has it all — in two great sizes.',
       isFavorite : false,
-      isInCart : false
+      isInCart : false,
+      quantity : 1,
     },
     {
       category : 'iPhone',
@@ -130,7 +142,8 @@ export const globalProductList = {
       price : 618,
       description : "Simply put your old iPhone next to your new one, and with a few taps, you can transfer your data automatically. If you need it, you can even get temporary iCloud storage for free to hold everything during the transfer.",
       isFavorite : false,
-      isInCart : false
+      isInCart : false,
+      quantity : 1,
     },
   ],
   'iPad': [
@@ -142,7 +155,8 @@ export const globalProductList = {
       price : 948,
       description : "The ultimate iPad experience. Now with breakthrough M1 performance, a breathtaking XDR display, and blazing‑fast 5G wireless. Buckle up.",
       isFavorite : false,
-      isInCart : false
+      isInCart : false,
+      quantity : 1,
     },
     {
       category : 'iPad',
@@ -152,7 +166,8 @@ export const globalProductList = {
       price : 748,
       description : "iPad Air lets you immerse yourself in whatever you’re reading, watching, or creating. The 10.9-inch Liquid Retina display features advanced technologies like True Tone, P3 wide color, and an antireflective coating.",
       isFavorite : false,
-      isInCart : false
+      isInCart : false,
+      quantity : 1,
     },
     {
       category : 'iPad',
@@ -162,7 +177,8 @@ export const globalProductList = {
       price : 398,
       description : "Powerful. Easy to use. Versatile. The new iPad is designed for all the things you love to do. Work, play, create, learn, stay connected, and more. All at an incredible value.",
       isFavorite : false,
-      isInCart : false
+      isInCart : false,
+      quantity : 1,
     },
     {
       category : 'iPad',
@@ -172,7 +188,8 @@ export const globalProductList = {
       price : 598,
       description : "iPad mini is meticulously designed to be absolutely beautiful. An all-new enclosure features a new, larger edge-to-edge screen, along with narrow borders and elegant rounded corners.",
       isFavorite : false,
-      isInCart : false
+      isInCart : false,
+      quantity : 1,
     },
   ],
   'Watch': [
@@ -185,6 +202,7 @@ export const globalProductList = {
       description : "The larger display enhances the entire experience, making Apple Watch easier to use and read. Series 7 represents our biggest and brightest thinking.",
       isFavorite : false,
       isInCart : false,
+      quantity : 1,
     },
     {
       category : 'Watch',
@@ -195,6 +213,7 @@ export const globalProductList = {
       description : "Powerful features to help keep you connected, active, healthy, and safe. Advanced sensors to track all the ways you move and to measure your favorite workouts. And available cellular so you can go without your phone. Apple Watch SE is a lot of watch for a lot less than you expected.",
       isFavorite : false,
       isInCart : false,
+      quantity : 1,
     },
     {
       category : 'Watch',
@@ -205,6 +224,7 @@ export const globalProductList = {
       description : "Splashproof. Swimproof. Surfproof.",
       isFavorite : false,
       isInCart : false,
+      quantity : 1,
     },
     {
       category : 'Watch',
@@ -215,6 +235,7 @@ export const globalProductList = {
       description : "Bounce around. Apple Watch Nike was made to move with you. And the new Bounce watch face does just that. It’s interactive and reacts to your motion. Customize it with your favorite colors or gradient to make exercising with Apple Watch a truly moving experience.",
       isFavorite : false,
       isInCart : false,
+      quantity : 1,
     },
     {
       category : 'Watch',
@@ -225,6 +246,7 @@ export const globalProductList = {
       description : "The exclusive Hermès Sport Band. Light, flexible, and water resistant, the Sport Band in signature Hermès orange is perfect for exercising. Included as an additional band with every Apple Watch Hermès silver stainless steel case. And space black stainless steel cases come with a matching black Hermès Sport Band.",
       isFavorite : false,
       isInCart : false,
+      quantity : 1,
     }
   ],
 
